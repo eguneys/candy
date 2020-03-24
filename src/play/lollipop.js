832553e4eabaacf16b907f3edbc13551f35a6191
Emre Guneyler <eguneys@hotmail.com>
2020-03-24 19:48:40 +0000

    releaes lollipop gang

--- src/play/lollipop.js
+++ src/play/lollipop.js
@@ -14,9 +14,50 @@ export default function Lollipop(play, ctx, bs) {
 
   const { width } = bs;
 
-  let pool = new Pool(() => new LollipopBody(this, ctx, bs));
+  let pool = new Pool(() => new LollipopGang(this, ctx, bs));
 
   this.init = data => {
+  };
+
+  this.release = (gang) => {
+    pool.release(gang);
+  };
+
+  const spawn = () => {
+    let x = mu.rand(0, width - 32);
+    pool.acquire(_ => _.init({x}));
+  };
+
+  const maybeSpawn = withDelay(() => {
+    spawn();
+  }, 10000);
+
+  this.update = delta => {
+    maybeSpawn(delta);
+    pool.each(_ => _.update(delta));
+  };
+
+
+  this.render = () => {
+    pool.each(_ => _.render());
+  };
+  
+}
+
+function LollipopGang(play, ctx, bs) {
+
+  const { width } = bs;
+
+  let pool = new Pool(() => new LollipopBody(this, ctx, bs));
+
+  let spawnCount;
+
+  let x;
+
+
+  this.init = data => {
+    spawnCount = 0;
+    x = data.x;
   };
 
   const spawn = (x) => {    
@@ -28,41 +69,31 @@ export default function Lollipop(play, ctx, bs) {
     pool.release(bullet);
   };
 
-  const maybeSpawnNone = withDelay(() => {
-  }, 10000);
-
-  let spawnFunctionCustomData;
-
-  let spawnFunctionResetCounter;
-  let spawnFunction = maybeSpawnNone;
-
-  const maybeSpawnOne = withDelay(() => {
-    spawnFunctionResetCounter--;
-    spawn(spawnFunctionCustomData);
-    if (spawnFunctionResetCounter < 0) {
-      spawnFunction = maybeSpawnNone;
-    }
-  }, 500);
-
   const maybeSpawn = withDelay(() => {
-    spawnFunctionResetCounter = 5;
-    spawnFunctionCustomData = mu.rand(32 * 2, width - 32 * 2);
-    spawnFunction = maybeSpawnOne;
-  }, 10000);
+    if (spawnCount++ < 5) {
+      spawn(x);
+    }
+  }, 200);
+
+  const maybeRelease = () => {
+    if (spawnCount > 0 && pool.alives() === 0) {
+      play.release(this);
+    }
+  };
 
   this.update = delta => {
     maybeSpawn(delta);
-    spawnFunction(delta);
-
+    maybeRelease();
     pool.each(_ => _.update(delta));
   };
 
 
   this.render = () => {
-    pool.each(_ => _.render());
-  };
-  
+    pool.each(_ => _.render());    
+  };  
 }
+
+
 
 function LollipopBody(play, ctx, bs) {
 
@@ -82,23 +113,18 @@ function LollipopBody(play, ctx, bs) {
   dBg.width = 32;
   dBg.height = 32;
 
-  let dBg2;
-  dBg2 = sprite(frames['candy']);
-  dBg2.width = 32;
-  dBg2.height = 32;
-
   let path1 = new CandyPath({ easing: Easings.easeOutQuad,
                               updateRate: SlowUpdateRate }),
-      path2 = new CandyPath({ updateRate: FastUpdateRate }),
+      path2 = new CandyPath({ easing: Easings.easeInQuad,
+                              updateRate: FastUpdateRate }),
       path = new PathCombined();
 
   this.init = data => {
     let x = data.x;
 
     oneLayer.add(dBg);
-    oneLayer.add(dBg2);
 
-    let edgeX = x < width * 0.5 ? 0 : width;
+    let edgeX = x < width * 0.5 ? width : 0;
 
     path1.init(x, - 32,
                x, height - 32);
@@ -107,8 +133,16 @@ function LollipopBody(play, ctx, bs) {
     path.init([path1, path2]);
   };
 
+  const release = () => {
+    dBg.remove();
+    play.release(this);
+  };
+
   this.update = delta => {
     path.update(delta);
+    if (path2.settled()) {
+      release(); 
+    }
   };
 
 
